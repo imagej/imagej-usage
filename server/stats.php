c6c61fbff4b3c8fa0fa3e8e922ca8de481fe8768
Curtis Rueden <ctrueden@wisc.edu>
2014-08-08 14:38:25 -0500

    stats.php: update schema to include configuration
    This includes:
    - country, language and timezone
    - operating system name, architecture, version
    - java configuration profile
    
    This change also separates the user tokens into their own users table,
    to avoid redundancy in the events table.

--- server/stats.php
+++ server/stats.php
@@ -51,9 +51,17 @@ function processStats($data) {
 		return;
 	}
 	$timestamp = date('Y-m-d H:i:s');
-	$user = value($json, 'user');
 	$ip_address = $_SERVER['REMOTE_ADDR'];
-	$event_id = insertEvent($db, $timestamp, $user, $ip_address);
+
+	$user_id = lookupUser($db, $json);
+	$country_id = lookupCountry($db, $json);
+	$language_id = lookupLanguage($db, $json);
+	$timezone_id = lookupTimezone($db, $json);
+	$os_id = lookupOS($db, $json);
+	$java_id = lookupJava($db, $json);
+
+	$event_id = insertEvent($db, $timestamp, $ip_address, $user_id,
+		$country_id, $language_id, $timezone_id, $os_id, $java_id);
 
 	foreach ($json['sites'] as &$site) {
 		$site_id = lookupSite($db, $site);
@@ -76,13 +84,44 @@ function connectToDB() {
 	$db = new mysqli($mysql_server, $mysql_user, $mysql_pass, $mysql_db);
 	if ($db->connect_errno) return null;
 
-	createTable($db, "sites", "site_id",
-		"name TINYTEXT, url TINYTEXT");
+	// list of users
+	createTable($db, "users", "user_id", "user TINYTEXT");
+
+	// list of countries
+	createTable($db, "countries", "country_id", "name TINYTEXT");
+
+	// list of languages
+	createTable($db, "languages", "language_id", "name TINYTEXT");
+
+	// list of timezones
+	createTable($db, "timezones", "timezone_id", "name TINYTEXT");
+
+	// operating systems
+	createTable($db, "os", "os_id",
+		"name TINYTEXT, arch TINYTEXT, version TINYTEXT");
+
+	// java installation profiles
+	createTable($db, "java", "java_id",
+		"runtime_name TINYTEXT, runtime_version TINYTEXT, " .
+		"spec_name TINYTEXT, spec_vendor TINYTEXT, spec_version TINYTEXT, " .
+		"vendor TINYTEXT, version TINYTEXT, " .
+		"vm_name TINYTEXT, vm_vendor TINYTEXT, vm_version TINYTEXT, " .
+		"vm_spec_name TINYTEXT, vm_spec_vendor TINYTEXT, vm_spec_version TINYTEXT");
+
+	// ImageJ update sites
+	createTable($db, "sites", "site_id", "name TINYTEXT, url TINYTEXT");
+
+	// identifiable objects (e.g., plugins)
 	createTable($db, "objects", "object_id",
 		"identifier TINYTEXT, site_id INT, version TINYTEXT, " .
 		"name TINYTEXT, label TINYTEXT, description TEXT");
+
+	// upload events (one row each time ImageJ uploads a batch of statistics)
 	createTable($db, "events", "event_id",
-		"timestamp DATETIME, user TINYTEXT, ip_address TINYTEXT");
+		"timestamp DATETIME, ip_address TINYTEXT, user_id INT, country_id INT, " .
+		"language_id INT, timezone_id INT, os_id INT, java_id INT");
+
+	// usage counts (per object, per event)
 	createTable($db, "stats", "stat_id",
 		"event_id INT, object_id INT, count INT");
 
@@ -96,6 +135,131 @@ function createTable($db, $table_name, $id_column, $columns) {
 		"PRIMARY KEY($id_column), $columns)";
 	$result = $db->query($sql);
 	if (!$result) die("Error creating table: $db->error");
+}
+
+/* Gets the user_id of a user, creating it if needed. */
+function lookupUser($db, $json) {
+	$user = value($json, 'user');
+
+	$statement = $db->prepare("SELECT user_id FROM users WHERE user = ?");
+	$statement->bind_param('s', $user);
+	$user_id = select($statement, 'user_id');
+	if ($user_id) return $user_id;
+
+	$statement = $db->prepare("INSERT INTO users (user) VALUES (?)");
+	$statement->bind_param('s', $user);
+	return insert($db, $statement);
+}
+
+/* Gets the country_id of a country, creating it if needed. */
+function lookupCountry($db, $json) {
+	$name = value($json, 'user_country');
+
+	$statement = $db->prepare("SELECT country_id FROM countries WHERE name = ?");
+	$statement->bind_param('s', $name);
+	$country_id = select($statement, 'country_id');
+	if ($country_id) return $country_id;
+
+	$statement = $db->prepare("INSERT INTO countries (name) VALUES (?)");
+	$statement->bind_param('s', $name);
+	return insert($db, $statement);
+}
+
+/* Gets the language_id of a language, creating it if needed. */
+function lookupLanguage($db, $json) {
+	$name = value($json, 'user_language');
+
+	$statement = $db->prepare("SELECT language_id FROM languages WHERE name = ?");
+	$statement->bind_param('s', $name);
+	$language_id = select($statement, 'language_id');
+	if ($language_id) return $language_id;
+
+	$statement = $db->prepare("INSERT INTO languages (name) VALUES (?)");
+	$statement->bind_param('s', $name);
+	return insert($db, $statement);
+}
+
+/* Gets the timezone_id of a timezone, creating it if needed. */
+function lookupTimezone($db, $json) {
+	$name = value($json, 'user_timezone');
+
+	$statement = $db->prepare("SELECT timezone_id FROM timezones WHERE name = ?");
+	$statement->bind_param('s', $name);
+	$timezone_id = select($statement, 'timezone_id');
+	if ($timezone_id) return $timezone_id;
+
+	$statement = $db->prepare("INSERT INTO timezones (name) VALUES (?)");
+	$statement->bind_param('s', $name);
+	return insert($db, $statement);
+}
+
+/* Gets the os_id of an OS configuration, creating it if needed. */
+function lookupOS($db, $json) {
+	$name = value($json, 'os_name');
+	$arch = value($json, 'os_arch');
+	$version = value($json, 'os_version');
+
+	$statement = $db->prepare("SELECT os_id FROM os WHERE " .
+		"name = ? AND arch = ? AND version = ?");
+	$statement->bind_param('sss', $name, $arch, $version);
+	$os_id = select($statement, 'os_id');
+	if ($os_id) return $os_id;
+
+	$statement = $db->prepare("INSERT INTO os " .
+		"(name, arch, version) VALUES (?, ?, ?)");
+	$statement->bind_param('sss', $name, $arch, $version);
+	return insert($db, $statement);
+}
+
+/* Gets the java_id of a java configuration, creating it if needed. */
+function lookupJava($db, $json) {
+	$runtime_name = value($json, 'java_runtime_name');
+	$runtime_version = value($json, 'java_runtime_version');
+	$spec_name = value($json, 'java_specification_name');
+	$spec_vendor = value($json, 'java_specification_vendor');
+	$spec_version = value($json, 'java_specification_version');
+	$vendor = value($json, 'java_vendor');
+	$version = value($json, 'java_version');
+	$vm_name = value($json, 'java_vm_name');
+	$vm_vendor = value($json, 'java_vm_vendor');
+	$vm_version = value($json, 'java_vm_version');
+	$vm_spec_name = value($json, 'java_vm_specification_name');
+	$vm_spec_vendor = value($json, 'java_vm_specification_vendor');
+	$vm_spec_version = value($json, 'java_vm_specification_version');
+
+	$statement = $db->prepare("SELECT java_id FROM java WHERE " .
+		"runtime_name = ? AND runtime_version = ? AND " .
+		"spec_name = ? AND spec_vendor = ? AND spec_version = ? AND " .
+		"vendor = ? AND version = ? AND " .
+		"vm_name = ? AND vm_vendor = ? AND vm_version = ? AND " .
+		"vm_spec_name = ? AND vm_spec_vendor = ? AND vm_spec_version = ?");
+	$statement->bind_param('sssssssssssss',
+		$runtime_name, $runtime_version,
+		$spec_name, $spec_vendor, $spec_version,
+		$vendor, $version,
+		$vm_name, $vm_vendor, $vm_version,
+		$vm_spec_name, $vm_spec_vendor, $vm_spec_version);
+	$java_id = select($statement, 'java_id');
+	if ($java_id) return $java_id;
+
+	$statement = $db->prepare("INSERT INTO java " .
+		"(runtime_name, runtime_version, " .
+		"spec_name, spec_vendor, spec_version, " .
+		"vendor, version, " .
+		"vm_name, vm_vendor, vm_version, " .
+		"vm_spec_name, vm_spec_vendor, vm_spec_version) " .
+		"VALUES (?, ?, " . // runtime: name, version
+		"?, ?, ?, " . // spec: name, vendor, version
+		"?, ?, " . // vendor, version
+		"?, ?, ?, " . // vm: name, vendor, version
+		"?, ?, ?)"); // vm_spec: name, vendor, version
+	$statement->bind_param('sssssssssssss',
+		$runtime_name, $runtime_version,
+		$spec_name, $spec_vendor, $spec_version,
+		$vendor, $version,
+		$vm_name, $vm_vendor, $vm_version,
+		$vm_spec_name, $vm_spec_vendor, $vm_spec_version);
+	return insert($db, $statement);
 }
 
 /* Gets the site_id of an update site, creating it if needed. */
@@ -115,14 +279,21 @@ function lookupSite($db, $site) {
 }
 
 /* Inserts a row into the events table, returning the new event ID. */
-function insertEvent($db, $timestamp, $user, $ip_address) {
+function insertEvent($db, $timestamp, $ip_address, $user_id,
+	$country_id, $language_id, $timezone_id, $os_id, $java_id)
+{
 	$statement = $db->prepare("INSERT INTO events " .
-		"(timestamp, user, ip_address) VALUES (?, ?, ?)");
-	$statement->bind_param('sss', $timestamp, $user, $ip_address);
+		"(timestamp, ip_address, user_id, " .
+		"country_id, language_id, timezone_id, os_id, java_id) " .
+		"VALUES (?, ?, ?, " . // timestamp, ip_address, user_id
+		"?, ?, ?, ?, ?)"); // country_id, language_id, timezone_id, os_id, java_id
+	$statement->bind_param('ssiiiiii',
+		$timestamp, $ip_address, $user_id,
+		$country_id, $language_id, $timezone_id, $os_id, $java_id);
 	return insert($db, $statement);
 }
 
-/** Inserts a row into the stats table, returning the new stat ID. */
+/* Inserts a row into the stats table, returning the new stat ID. */
 function insertStat($db, $event_id, $stat, $site_id) {
 	$identifier = value($stat, 'id');
 	$object_id = lookupObject($db, $stat, $site_id);
